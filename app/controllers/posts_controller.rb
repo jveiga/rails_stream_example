c9c70ef070f01ced4ca4b4d648c61e4d0c1d3e3f
João Veiga <styluss@gmail.com>
2015-11-15 21:21:56 +0100

    add comment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,8 @@ class PostsController < ApplicationController
   def index
     response.headers["Content-Type"] = "text/event-stream"
     loop do
+      # you might want to add a \n at the end
+      # of every write, for the client to be able use ReadLine or smt
       data = {data: Time.now, id: 42 }.to_json
       response.stream.write data
       sleep 0.1
